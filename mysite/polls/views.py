488036c0b1ad1784bc352b93f2bd247547c4303d
Chenzhi Zhu <zhucz16@mails.tsinghua.edu.cn>
2018-07-19 17:18:14 +0800

    Fixed some bugs

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
